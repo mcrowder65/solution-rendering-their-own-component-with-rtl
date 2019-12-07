c5b469b73b97b296e3a5a1c9da378c3d76dbfe8c
Matt Crowder <mcrowder65@gmail.com>
2019-12-07 14:51:29 -0500

    updated diameter

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ import Circle from "./circle"
 function App() {
   return (
     <div>
-      <Circle dimensions={{ diameter: 200 }} backgroundColor="red" />
-      <Circle dimensions={{ diameter: 400 }} />
+      <Circle dimensions={{ diameter: 50 }} backgroundColor="red" />
+      <Circle dimensions={{ diameter: 150 }} />
     </div>
   )
 }
